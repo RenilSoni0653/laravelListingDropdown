d8a0bd881202052eef1effd1343e7e696f8ed2df
Renil <deep@moweb.com>
2021-05-24 15:59:06 +0530

    modified dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,6 @@
         });
     </script>
     <!-- End of tinymce -->
-
     <!-- Dropzone Script -->
     {{-- CSS assets in head section --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
